9594dda2ec9caa76e5ea453c2578b71cf84adc5f
Andrew Hird <andrew.james.hird@gmail.com>
2016-10-31 17:35:10 +1300

    tip percentage working

--- index.ios.js
+++ index.ios.js
@@ -4,30 +4,117 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   AppRegistry,
   StyleSheet,
   Text,
+  TextInput,
   View
-} from 'react-native';
+} from 'react-native'
+
+let entered = {
+  price: '20',
+  percentage: '18',
+  tax: '4',
+  rate: '1.39'
+}
 
 export default class tipped extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      price: '20',
+      percentage: '18',
+      tax: '4',
+      conversionRate: '1.39',
+      total: '34.30'
+    }
+  }
+
+  enterPrice (price) {
+    entered.price = price
+    this.calc()
+  }
+
+  enterPercentage (percentage) {
+    entered.percentage = percentage
+    this.calc()
+  }
+
+  enterTax (tax) {
+    entered.tax = tax
+    this.calc()
+  }
+
+  enterRate (conversionRate) {
+    entered.conversionRate = conversionRate
+    console.log(entered)
+    this.calc()
+  }
+
+  calc () {
+    let totalPrice = 0
+    totalPrice = entered.price * (entered.percentage/ 100+1)
+    this.state = {
+      price: entered.price,
+      percentage: entered.percentage,
+      tax: entered.tax,
+      conversionRate: entered.conversionRate,
+      total: totalPrice }
+    console.log(totalPrice)
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Tipped
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Text>Enter price:</Text>
+        <TextInput
+          keyboardType='numeric'
+          style={styles.input}
+          onChangeText={(price) => {
+            this.setState({price})
+            this.enterPrice(price)
+          }}
+          value={this.state.price}
+        />
+        <Text>Enter tip percentage:</Text>
+        <TextInput
+          keyboardType='numeric'
+          style={styles.input}
+          onChangeText={(percentage) => {
+            this.setState({percentage})
+            this.enterPercentage(percentage)
+          }}
+          value={this.state.percentage}
+        />
+        <Text>Enter state tax:</Text>
+        <TextInput
+          keyboardType='numeric'
+          style={styles.input}
+          onChangeText={(tax) => {
+            this.setState({tax})
+            this.enterTax(tax)
+          }}
+          value={this.state.tax}
+        />
+        <Text>Enter conversion rate:</Text>
+        <TextInput
+          keyboardType='numeric'
+          style={styles.input}
+          onChangeText={(conversionRate) => {
+            this.setState({conversionRate})
+            this.enterRate(conversionRate)
+          }}
+          value={this.state.conversionRate}
+        />
+        <Text style={styles.total}>Total Price: ${this.state.total}</Text>
       </View>
-    );
+    )
   }
 }
 
@@ -48,6 +135,15 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  input: {
+    textAlign: 'center',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1
+  },
+  total: {
+    fontSize: 30
+  }
 });
 
-AppRegistry.registerComponent('tipped', () => tipped);
+AppRegistry.registerComponent('tipped', () => tipped)
