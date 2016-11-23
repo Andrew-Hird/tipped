c2d72fe5d150d1dfea4e2c4d9c47aefeb9ab4167
Andrew Hird <andrew.james.hird@gmail.com>
2016-11-23 15:16:10 +1300

    put styles in seperate file

---
+++ Styles.js
@@ -0,0 +1,74 @@
+'use strict';
+
+import React from 'react'
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  scrollView: {
+    backgroundColor: 'steelblue',
+    height: 300
+  },
+  horizontalScrollView: {
+    height: 120
+  },
+  container: {
+    flex: 1,
+    marginTop: 20,
+    // height: 1136,
+    alignItems: 'center',
+    backgroundColor: 'steelblue'
+  },
+  allTotals: {
+
+  },
+  heading: {
+    padding: 10,
+    fontSize: 15
+  },
+  num: {
+    height: 125,
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+  input: {
+    marginLeft: 45,
+    textAlign: 'center',
+    height: 80,
+    width: 80,
+    fontSize: 25,
+    borderColor: 'black',
+    borderWidth: 1
+  },
+  item: {
+    width: 150,
+    alignItems: 'center',
+  },
+  rate: {
+    fontSize: 25
+  },
+  totalHead: {
+    marginBottom: 5,
+    fontSize: 30,
+    textAlign: 'center',
+    textDecorationLine: 'underline',
+  },
+  total: {
+    fontSize: 26,
+    marginBottom: 15,
+    textAlign: 'right'
+  },
+  title: {
+    fontSize: 40,
+    marginBottom: 10
+  },
+  button: {
+    flex: 1,
+   height:60,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+   fontSize:20
+  }
+})
--- components/Credit-Card.js
+++ components/Credit-Card.js
@@ -12,6 +12,8 @@ import {
 
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view'
 
+import styles from '../Styles.js'
+
 let NZD = 0
 
 let conversionFee = '2.10'
@@ -147,73 +149,3 @@ export class CreditCard extends Component {
     )
   }
 }
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: 'steelblue',
-    height: 300
-  },
-  horizontalScrollView: {
-    height: 120
-  },
-  container: {
-    flex: 1,
-    marginTop: 20,
-    // height: 1136,
-    alignItems: 'center',
-    backgroundColor: 'steelblue'
-  },
-  allTotals: {
-
-  },
-  heading: {
-    padding: 10,
-    fontSize: 15
-  },
-  num: {
-    height: 125,
-    flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap'
-  },
-  input: {
-    marginLeft: 45,
-    textAlign: 'center',
-    height: 80,
-    width: 80,
-    fontSize: 25,
-    borderColor: 'black',
-    borderWidth: 1
-  },
-  item: {
-    width: 150,
-    alignItems: 'center',
-  },
-  rate: {
-    fontSize: 25
-  },
-  totalHead: {
-    marginBottom: 5,
-    fontSize: 30,
-    textAlign: 'center',
-    textDecorationLine: 'underline',
-  },
-  total: {
-    fontSize: 26,
-    marginBottom: 15,
-    textAlign: 'right'
-  },
-  title: {
-    fontSize: 40,
-    marginBottom: 10
-  },
-  button: {
-    flex: 1,
-  	height:60,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  buttonText: {
-  	fontSize:20
-  }
-})
